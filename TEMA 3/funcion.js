dd837446d75fde3a615fdcee7dbb64da0fdcefb5
Miguel Luque Humanes <miguelluque05@iesamachado.org>
2023-12-15 14:18:17 +0100

    clase 15/12/23

---
+++ TEMA 3/funcion.js
@@ -0,0 +1,7 @@
+/*Conjunto de funciones
+realizadas por Miguel Luque
+para el sitio web http://www.dominio.com
+*/
+function muestraPopup() {
+    alert("Hola hackers!!!!") //Esto es un comentario de una línea//
+}
