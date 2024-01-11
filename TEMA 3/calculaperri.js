e162b52328b427eb3e7d9f9cda231900d8ab614c
Miguel Luque Humanes <miguelluque05@iesamachado.org>
2024-01-11 13:21:36 +0100

    clase 11 enero

---
+++ TEMA 3/calculaperri.js
@@ -0,0 +1,28 @@
+function suma(){
+            var n1 = parseFloat(document.getElementById("n1").value);
+            var n2 = parseFloat(document.getElementById("n2").value);
+            document.getElementById("resultado").innerHTML = n1+n2;
+        }
+
+        function resta(){
+            var n1 = parseFloat(document.getElementById("n1").value);
+            var n2 = parseFloat(document.getElementById("n2").value);
+            document.getElementById("resultado").innerHTML = n1-n2;
+        }
+
+        function division(){
+            var n1 = parseFloat(document.getElementById("n1").value);
+            var n2 = parseFloat(document.getElementById("n2").value);
+            if (n2==0){
+                alert ("Imposible dividir por cero");
+            }
+            else{
+                document.getElementById("resultado").innerHTML = n1/n2;
+            }
+        }
+
+        function multiplicacion(){
+            var n1 = parseFloat(document.getElementById("n1").value);
+            var n2 = parseFloat(document.getElementById("n2").value);
+            document.getElementById("resultado").innerHTML = n1*n2;
+        }
---
+++ TEMA 3/circulos.js
@@ -0,0 +1,26 @@
+/*
+Versión cutre
+function ocultarRojo(){
+    document.getElementById("rojo").style.visibility = "hidden"; display = "none"
+}
+function ocultarAzul(){
+    document.getElementById("azul").style.visibility = "hidden"; display = "none"
+}
+function ocultarVerde(){
+    document.getElementById("verde").style.visibility = "hidden"; display = "none"
+}
+*/
+/*Versión mejorada */
+function ocultar(caja){
+    document.getElementById(caja).style.visibility ="hidden"; 
+}
+function mostrar(caja){
+    document.getElementById(caja).style.visibility ="visible"; 
+}
+/*Versión con un solo botón*/
+function ocultar_o_mostrar(caja){
+    if (document.getElementById(caja).style.visibility== "visible");
+       document.getElementById(caja).style.visibility = "hidden";
+    else
+        document.getElementById(caja).style.visibility = "visible";
+}
